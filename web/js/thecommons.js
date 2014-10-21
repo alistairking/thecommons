ef22ae6b8460a505e7bc827c6b6a4d7740db5bba
Alistair King <alistair@caida.org>
2014-10-20 18:41:07 -0700

    prevent scrolling code calculating height of non-existent elements

--- web/js/thecommons.js
+++ web/js/thecommons.js
@@ -14,8 +14,8 @@ $(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            var offset = target.offset().top + wrapperDiv.scrollTop();
             if (target.length) {
+                var offset = target.offset().top + wrapperDiv.scrollTop();
                 wrapperDiv.stop().animate({
                     scrollTop: offset
                 }, 300);
