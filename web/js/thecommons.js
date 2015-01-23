161a7b7566791591549959720ca2c75f30d9a42c
Alistair King <alistair@caida.org>
2015-01-23 07:42:58 -0800

    fixes and improments for use on mobile

--- web/js/thecommons.js
+++ web/js/thecommons.js
@@ -8,16 +8,19 @@ $(function () {
     var currentPage = location.hash ? location.hash : '#home';
     $('.tc-sidebar li > a[href="'+currentPage+'"]').parent().addClass("active");
 
+    var sidebar = $('.tc-sidebar');
+
     var body = $('html body');
     var anchorLinks = $('.tc-sidebar a[href*=#]:not([href=#give])');
     anchorLinks.click(function (e) {
         e.preventDefault();
+        sidebar.toggleClass('tc-sidebar-hidden');
+
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                var offset = target.offset().top;
-                console.log('offset: ' + offset);
+                var offset = target.offset().top - $('.tc-topbar').height() + 1;
                 body.stop().animate({
                     scrollTop: offset
                 }, 300);
@@ -28,5 +31,13 @@ $(function () {
             }
         }
         return true;
+    });
+
+    $('.tc-sidebar-toggle').click(function(e) {
+        sidebar.toggleClass('tc-sidebar-hidden');
+    });
+
+    $('.container-fluid').click(function(e) {
+        sidebar.addClass('tc-sidebar-hidden');
     });
 });
