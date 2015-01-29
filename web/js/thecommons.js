b8cf07e95cabd76c5b68ccbe3a721cc5c5f4a45f
Alistair King <alistair@caida.org>
2015-01-29 15:57:25 -0800

    fixes to scrolling js code

--- web/js/thecommons.js
+++ web/js/thecommons.js
@@ -3,15 +3,26 @@
  */
 
 /* smooth scrolling on anchor jumps */
-$(function () {
+$(window).load(function () {
     /* set the current hash as active */
-    var currentPage = location.hash ? location.hash : '#home';
-    $('.tc-sidebar li > a[href="'+currentPage+'"]').parent().addClass("active");
+    //var currentPage = location.hash ? location.hash : '#home';
+    //$('.tc-sidebar li > a[href="'+currentPage+'"]').parent().addClass("active");
 
     var sidebar = $('.tc-sidebar');
 
     var body = $('html body');
     var anchorLinks = $('.tc-sidebar a[href*=#]:not([href=#give])');
+
+    function scrollTo(target, link) {
+        var offset = target.offset().top - $('.tc-topbar').height();// + 1;
+        body.stop().animate({
+            scrollTop: offset
+        }, 300);
+        history.pushState({}, '', link.hash);
+        anchorLinks.parent().removeClass("active");
+        $(link).parent().addClass("active");
+    }
+
     anchorLinks.click(function (e) {
         e.preventDefault();
         sidebar.toggleClass('tc-sidebar-hidden');
@@ -20,18 +31,15 @@ $(function () {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                var offset = target.offset().top - $('.tc-topbar').height();// + 1;
-                body.stop().animate({
-                    scrollTop: offset
-                }, 300);
-                history.pushState({}, '', this.hash);
-                anchorLinks.parent().removeClass("active");
-                $(this).parent().addClass("active");
+                scrollTo(target, this);
                 return false;
             }
         }
         return true;
     });
+
+    var currentPage = location.hash ? location.hash : '#home';
+    scrollTo($(currentPage), $('.tc-sidebar li > a[href="' + currentPage + '"]'));
 
     $('.tc-sidebar-toggle').click(function(e) {
         sidebar.toggleClass('tc-sidebar-hidden');
