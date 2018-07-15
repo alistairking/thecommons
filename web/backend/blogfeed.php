3bc89b106c44df052f2783617d63e9f55996723b
Alistair King <alistair@caida.org>
2018-07-15 09:37:46 -0400

    Start adding a blog feed panel

---
+++ web/backend/blogfeed.php
@@ -0,0 +1,27 @@
+<?php
+
+# Open the cURL session
+$curlSession = curl_init();
+curl_setopt($curlSession, CURLOPT_URL, "http://blog.thecommons.church/feed/json");
+curl_setopt($curlSession, CURLOPT_HEADER, 1);
+curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curlSession, CURLOPT_TIMEOUT, 30);
+curl_setopt($curlSession, CURLOPT_SSL_VERIFYHOST, 1);
+# Send the request and store the result in an array
+$response = curl_exec($curlSession);
+# Check that a connection was made
+if (curl_error($curlSession)) {
+    # If it wasn't...
+    print curl_error($curlSession);
+} else {
+    #clean duplicate header that seems to appear on fastcgi with output buffer on some servers!!
+    $response = str_replace("HTTP/1.1 100 Continue\r\n\r\n", "", $response);
+    $ar = explode("\r\n\r\n", $response, 2);
+    $header = $ar[0];
+    $body = $ar[1];
+    $data = $body;
+}
+# Output the JSONP callback with data
+header('Content-type: application/json');
+echo $data;
+
---
+++ web/js/blogfeed.js
@@ -0,0 +1,87 @@
+/**
+ * Created by alistair on 3/17/15.
+ */
+
+var MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+var BG_COLORS = [
+    "#231f20",
+    "#433f40",
+    "#635f60",
+];
+
+// (idx == 0 ? '<a href="#">Read More&nbsp;<i class="glyphicon glyphicon-chevron-right"></i></a>' : '') +
+
+var ARTICLE_CNT = 3;
+
+var articles = [];
+
+function expandArticle() {
+    var idx = $(this).attr("href").split("#")[1];
+    var articleId = "#blog-article-" + idx;
+    var $article = $(articleId);
+    // is it currently expanded?
+    var expanded = $article.hasClass("expanded");
+
+    if (expanded) {
+        $article.css("height", null);
+        $(articleId + " .content").addClass(idx == 0 ? "short" : "hidden");
+    } else {
+        $article.css("height", "unset");
+        $(articleId + " .content").removeClass(idx == 0 ? "short" : "hidden");
+    }
+
+    $article.toggleClass("expanded");
+    return false;
+}
+
+$(function () {
+    $.getJSON("/backend/blogfeed.php", function(data) {
+        var blogContent = $('#blog > .blog-content');
+        blogContent.html("");
+        var numArticles = Math.min(ARTICLE_CNT, data.items.length);
+        articles = data.items;
+
+        // TODO: fix linking content text in first article (should there be a link? should it be underlined? how does collapse work?)
+        // TODO: figure out how to get the height to work on expand
+
+        for (var idx = 0; idx < numArticles; idx++) {
+            var article = articles[idx];
+            var date = new Date(Date.parse(article.date_published));
+            var day = date.getDate();
+            var monthIndex = date.getMonth();
+            var year = date.getFullYear();
+            var dateStr = MONTHS[monthIndex] + " " + day + ", " + year;
+            var rowClass = idx === 0 ? "first" : "all";
+            var contentClass = idx === 0 ? "short" : "hidden";
+            blogContent.append(
+                '<div id="blog-article-' + idx + '" class="row blog-article ' + rowClass + '" style="background-color: ' + BG_COLORS[idx] + ';">' +
+                '<div>' +
+                '<div class="date">' + dateStr + '</div>' +
+                '<div class="title"><a href="#' + idx + '">' + article.title + '</a></div>' +
+                '<div class="content ' + contentClass + '">' +
+                (idx === 0 ? '<a href="#' + idx + '">' : '') +
+                article.content_html +
+                (idx === 0 ? '</a>' : '') +
+                '</div>' +
+                '</div>' +
+                '</div>'
+            );
+        }
+
+        $(".blog-article a").on("click", expandArticle);
+    });
+});
